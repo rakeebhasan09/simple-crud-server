52a42f28e5a49f2f76d2957e72bb14268b39f442
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-10-29 10:28:35 +0600

    Create post api for add user and save the user into mongodb atlas.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,9 +9,48 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// simple_crud_user
+// ZG7rhFURhWDjwET9
+const uri =
+	"mongodb+srv://simple_crud_user:ZG7rhFURhWDjwET9@cluster0.x65kkeb.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		const databaseName = client.db("simple_crud_db");
+		const usersCollection = databaseName.collection("users");
+
+		// User Post API
+		app.post("/users", async (req, res) => {
+			const newUserInfo = req.body;
+			const result = await usersCollection.insertOne(newUserInfo);
+			res.send(result);
+		});
+
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
