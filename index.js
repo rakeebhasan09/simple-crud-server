f4367aff462a5d440f300602d91837efe0a7e19d
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-10-29 11:32:33 +0600

    Create delete api for get users and send to client side.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -44,6 +44,14 @@ async function run() {
 		app.post("/users", async (req, res) => {
 			const newUserInfo = req.body;
 			const result = await usersCollection.insertOne(newUserInfo);
+			res.send(result);
+		});
+
+		// User Delete API
+		app.delete("/users/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await usersCollection.deleteOne(query);
 			res.send(result);
 		});
 
